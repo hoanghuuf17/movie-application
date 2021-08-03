575bb464225ae159bb4247a026af22384cd3c7e8
hoanghuuf17 <hoanghuuf17@gmail.com>
2021-08-04 01:18:29 +0700

    search screen UI base

--- src/screens/search/index.js
+++ src/screens/search/index.js
@@ -1,11 +1,91 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import {
+    Text,
+    StyleSheet,
+    TextInput,
+    View, SafeAreaView,
+    ScrollView,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    TouchableOpacity
+} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import styles from './styles';
 
 const SearchSreen = () => {
-    return (
-        <View style={{backgroundColor : '#21182C', height: '100%'}}>
+    const [data, setData] = useState([
+        {
+            name: 'Harry Potter 7',
+            rating: 4.5,
+            info: 'Eng | Fiction | 2h10m'
+        },
 
-        </View>
+        {
+            name: 'Iron Men 3',
+            rating: 5.0,
+            info: 'Eng | Action | 1h30m'
+        }
+    ])
+
+    const [result, setResult] = useState([])
+    const [input, setInput] = useState('')
+
+    const search = (text) => {
+        if (text) {
+            const newData = data.filter(
+                function (item) {
+                    const itemData = item.name
+                        ? item.name.toUpperCase()
+                        : ''.toUpperCase();
+                    const textData = text.toUpperCase();
+                    return itemData.indexOf(textData) > -1;
+                });
+            setResult(newData);
+            setInput(text);
+        } else {
+            setResult(data);
+            setInput(text);
+        }
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.box}>
+                <View style={styles.header}>
+                    <Text style={styles.title}>Hi, Hoang</Text>
+                </View>
+                <View style={styles.searchBar}>
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Search movie, series"
+                        placeholderTextColor="#ddd"
+                        onChangeText={(text) => search(text)}
+                    />
+                    <TouchableOpacity style={styles.btnSearch}>
+                        <FontAwesome name="search" size={20} color='white' />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.content}>
+                    <KeyboardAvoidingView
+                        behavior={Platform.OS === "ios" ? "padding" : "height"}
+                        style={styles.container}
+                        keyboardVerticalOffset={90}>
+                        <TouchableWithoutFeedback keyboard="dismiss">
+                            <>
+                                <ScrollView>
+                                    {data &&
+                                        data.map((doc, index) => {
+                                            const { name, rating, info } = doc;
+                                            return (<Text key={index}>{name}</Text>)
+                                        })}
+                                </ScrollView>
+                            </>
+                        </TouchableWithoutFeedback>
+                    </KeyboardAvoidingView>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 }
 
--- src/screens/search/styles.js
+++ src/screens/search/styles.js
@@ -1,5 +1,45 @@
 import { StyleSheet } from 'react-native';
 export default styles = StyleSheet.create({
-
-
+    container: {
+        display: 'flex',
+        flex: 1,
+        backgroundColor: '#21182C'
+    },
+    box: {
+        flex: 1
+    },
+    header: {
+        flex: 0.5,
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'white',
+        paddingLeft: 20
+    },
+    searchBar: {
+        top: -10,
+        margin: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1,
+    },
+    textInput: {
+        flex: 1,
+        padding: 10,
+        height: 40,
+        borderRadius: 20,
+        backgroundColor: '#7F7A84'
+    },
+    btnSearch:{
+        backgroundColor: '#FF6802',
+        padding:10, 
+        borderRadius: 50,
+        marginLeft : 10
+    },  
+    content: {
+        flex: 9,
+    }
 })
