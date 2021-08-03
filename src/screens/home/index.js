db65503c885b0d97c51420a88bfeee284d951158
hoanghuuf17 <hoanghuuf17@gmail.com>
2021-08-04 00:41:36 +0700

    Home Screen

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,11 +1,101 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView, ScrollView, Text, TouchableOpacity, Pressable, Image } from 'react-native';
+import styles from './styles';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
+const Captain = '../../images/captain3.jpg';
+const Hp = '../../images/hp7.png'
 
 const HomeSreen = () => {
+    const popular = [1, 2, 3];
     return (
-        <View style={{backgroundColor : '#21182C', height: '100%'}}>
-
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.box}>
+                <View style={styles.header}>
+                    <Text style={styles.title}>Hi, Hoang</Text>
+                </View>
+                <View style={styles.filter}>
+                    <Pressable style={[styles.filterPress, { backgroundColor: "#843623", padding: 10, borderRadius: 15 }]}>
+                        <Text style={styles.filterText}>All</Text>
+                    </Pressable>
+                    <Pressable style={styles.filterPress}>
+                        <Text style={styles.filterText}>Action</Text>
+                    </Pressable>
+                    <Pressable style={styles.filterPress}>
+                        <Text style={styles.filterText}>Comedy</Text>
+                    </Pressable>
+                    <Pressable style={styles.filterPress}>
+                        <Text style={styles.filterText}>Horor</Text>
+                    </Pressable>
+                    <TouchableOpacity style={styles.filterBtn}>
+                        <AntDesign name="filter" size={25} color="white" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.oustanding}>
+                    <Image style={styles.ostImg} source={require(Captain)} />
+                    <Text style={styles.ostName}>Captain Ameraica: civil war</Text>
+                    <Text style={styles.ostRating}><Entypo name="star" size={25} color="#EFCD09" /> 5.0</Text>
+                    <TouchableOpacity style={styles.playBtn}>
+                        <Entypo name="controller-play" size={28} color="#FF6802" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.popular}>
+                    <View style={styles.popularHeader}>
+                        <Text style={styles.popularTitle}>Popular</Text>
+                        <Entypo name="dots-three-horizontal" size={23} color="#FF6802" />
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <ScrollView
+                            contentContainerStyle={styles.scrStyle}
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}>
+                            {popular.map((index) => (
+                                <View style={styles.poItem} key={index}>
+                                    <Image style={styles.poImg} source={require(Hp)} />
+                                    <View style={styles.itemBar}>
+                                        <View style={{ flexDirection: 'column' }}>
+                                            <Text style={styles.itemName}>Harry Potter 7</Text>
+                                            <Text style={styles.itemRating}><Entypo name="star" size={18} color="#EFCD09" /> 4.5</Text>
+                                        </View>
+                                        <TouchableOpacity style={styles.itemplayBtn}>
+                                            <Entypo name="controller-play" size={18} color="#FF6802" />
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            ))}
+                        </ScrollView>
+                    </View>
+                </View>
+                <View style={styles.maylike}>
+                    <View style={styles.popularHeader}>
+                        <Text style={styles.popularTitle}>You may like</Text>
+                        <Entypo name="dots-three-horizontal" size={23} color="#FF6802" />
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <ScrollView
+                            contentContainerStyle={styles.scrStyle}
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}>
+                            {popular.map((index) => (
+                                <View style={styles.poItem} key={index}>
+                                    <Image style={[styles.poImg, { height: 130, width: 130 }]} source={require(Hp)} />
+                                    <View style={[styles.itemBar, {}]}>
+                                        <View style={{ flexDirection: 'column' }}>
+                                            <Text style={[styles.itemName, { fontWeight: '500', fontSize: 12, }]}>Harry Potter 7</Text>
+                                            <Text style={[styles.itemRating, { fontWeight: '500', fontSize: 12, }]}><Entypo name="star" size={15} color="#EFCD09" /> 4.5</Text>
+                                        </View>
+                                        <TouchableOpacity style={[styles.itemplayBtn, { marginLeft: 7, marginRight: 5, }]}>
+                                            <Entypo name="controller-play" size={18} color="#FF6802" />
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            ))}
+                        </ScrollView>
+                    </View>
+                </View>
+                <View style={styles.nothing}></View>
+            </View>
+        </SafeAreaView>
     );
 }
 
--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -1,5 +1,131 @@
 import { StyleSheet } from 'react-native';
 export default styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#1b1324',
+        flex: 1,
+    },
+    box: {
+        flex: 1,
+    },
+    header: {
+        flex: 0.75,
+        justifyContent: 'center',
+    },
+    filter: {
+        flex: 0.75,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingLeft: 20,
+        paddingRight: 20
 
-
+    },
+    oustanding: {
+        flex: 3,
+        padding: 20
+    },
+    popular: {
+        flex: 2.75,
+    },
+    maylike: {
+        flex: 2.25,
+    },
+    nothing: {
+        flex: 0.5,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'white',
+        paddingLeft: 20
+    },
+    filterText: {
+        fontSize: 20,
+        fontWeight: '600',
+        color: 'white',
+    },
+    filterPress: {
+        padding: 10
+    },
+    ostImg: {
+        width: '100%',
+        flex: 1,
+        borderRadius: 20,
+    },
+    ostName: {
+        color: 'white',
+        position: 'absolute',
+        bottom: 70,
+        left: 30,
+        fontWeight: '600',
+        fontSize: 20,
+    },
+    ostRating: {
+        color: 'white',
+        position: 'absolute',
+        bottom: 40,
+        left: 30,
+    },
+    playBtn: {
+        position: 'absolute',
+        bottom: 45,
+        right: 40,
+        backgroundColor: 'white',
+        borderRadius: 50,
+        alignItems: 'center',
+        padding: 3
+    },
+    popularHeader:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginLeft: 20,
+        marginRight: 20,
+    },  
+    popularTitle: {
+        fontSize: 20,
+        fontWeight: '600',
+        color: 'white',
+    },
+    poItem: {
+        alignItems: 'center',
+        borderRadius: 50,
+        marginRight: 10
+    },
+    poImg: {
+        borderRadius: 20,
+        height: 180,
+        width: 180,
+    },
+    scrStyle: {
+        alignItems: 'center',
+        marginLeft: 20
+    },
+    itemBar: {
+        borderRadius: 20,
+        position: 'absolute',
+        height: 'auto',
+        bottom: 5,
+        padding: 5,
+        flexDirection: 'row',
+        backgroundColor: '#FFFFFF50',
+        alignItems: 'center'
+    },
+    itemName: {
+        color: 'white',
+        fontWeight: '600',
+        fontSize: 15,
+    },
+    itemRating: {
+        color: 'white',
+        fontWeight : 'bold',
+        fontSize: 15,
+    },
+    itemplayBtn: {
+        backgroundColor: 'white',
+        borderRadius: 50,
+        alignItems: 'center',
+        height: 20,
+        marginLeft: 20,
+        marginRight: 10,
+    }
 })
