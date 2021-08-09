ac5fe50e49190a01592b4c9f52dfd066c4be14d9
hoanghuuf17 <hoanghuuf17@gmail.com>
2021-08-09 12:29:14 +0700

    webview

--- src/screens/detail/index.js
+++ src/screens/detail/index.js
@@ -1,5 +1,6 @@
 import React, { useLayoutEffect, useState } from 'react';
 import { View, SafeAreaView, TouchableOpacity, Image, Text, FlatList } from 'react-native';
+import { WebView } from 'react-native-webview';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Entypo from 'react-native-vector-icons/Entypo';
 import CastItem from '../../components/CastItem.js';
@@ -15,6 +16,7 @@ const DetailSreen = ({ route, navigation }) => {
 
     const { name, image, info, description, actors } = route.params
     const [heart, setHeart] = useState(false);
+    const [webView, setWebView] = useState(false);
 
     const addToFavorite = () => {
         dispatch(addFavorite({ name, image, info, description, actors }));
@@ -42,53 +44,57 @@ const DetailSreen = ({ route, navigation }) => {
             ),
         });
         list.find(mv => mv.name === name ? setHeart(true) : '')
-    }, [navigation, heart, list])
+    }, [navigation, heart])
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.box}>
-                <View style={styles.poster}>
-                    <Image style={styles.posterImg} source={{ uri: image }} />
-                    <Text style={styles.posterRating}><Entypo name="star" size={25} color="#EFCD09" /> 5.0</Text>
-                    <View style={styles.posterBgr}>
-                        <TouchableOpacity style={styles.playBtn}>
-                            <Entypo name="controller-play" size={28} color="#FF6802" />
-                        </TouchableOpacity>
-                        <Text style={styles.watchNow}>Watch now</Text>
+            {webView ? <WebView
+                incognito={true}
+                style={{ flex: 1 }}
+                source={{ url: `https://www.moviefone.com/search/${name}` }} /> :
+                <View style={styles.box}>
+                    <View style={styles.poster}>
+                        <Image style={styles.posterImg} source={{ uri: image }} />
+                        <Text style={styles.posterRating}><Entypo name="star" size={25} color="#EFCD09" /> 5.0</Text>
+                        <View style={styles.posterBgr}>
+                            <TouchableOpacity style={styles.playBtn} onPress={() => setWebView(!webView)}>
+                                <Entypo name="controller-play" size={28} color="#FF6802" />
+                            </TouchableOpacity>
+                            <Text style={styles.watchNow}>Watch now</Text>
+                        </View>
                     </View>
-                </View>
-                <View style={styles.infos}>
-                    <Text style={styles.name}>{name}</Text>
-                    <Text style={styles.info}>{info}</Text>
-                </View>
-                <View style={styles.line}></View>
-                <View style={styles.detail}>
-                    <Text style={styles.detailTitle}>Story line</Text>
-                    <Text style={styles.detailContent}>{description}</Text>
-                </View>
-                <View style={styles.cast}>
-                    <Text style={styles.castTitle}>Star cast</Text>
-                    <FlatList
-                        data={actors}
-                        renderItem={({ item }) => <CastItem item={item} />}
-                        showsHorizontalScrollIndicator={false}
-                        keyExtractor={item => item.id}
-                        horizontal={true}
-                    />
-                </View>
-                <View style={styles.recommended}>
-                    <Text style={styles.recommendedTitle}>Recommended</Text>
-                    <FlatList
-                        data={recommended}
-                        renderItem={({ item }) =>
-                            <RecommendItem item={item} size={130} onPress={() => navigation.navigate('Detail',
-                                { name: item.name, image: item.image, info: item.info, description: item.description, actors: item.actors })} />}
-                        showsHorizontalScrollIndicator={false}
-                        keyExtractor={({ id }) => id}
-                        horizontal={true}
-                    />
-                </View>
-            </View>
+                    <View style={styles.infos}>
+                        <Text style={styles.name}>{name}</Text>
+                        <Text style={styles.info}>{info}</Text>
+                    </View>
+                    <View style={styles.line}></View>
+                    <View style={styles.detail}>
+                        <Text style={styles.detailTitle}>Story line</Text>
+                        <Text style={styles.detailContent}>{description}</Text>
+                    </View>
+                    <View style={styles.cast}>
+                        <Text style={styles.castTitle}>Star cast</Text>
+                        <FlatList
+                            data={actors}
+                            renderItem={({ item }) => <CastItem item={item} />}
+                            showsHorizontalScrollIndicator={false}
+                            keyExtractor={item => item.id}
+                            horizontal={true}
+                        />
+                    </View>
+                    <View style={styles.recommended}>
+                        <Text style={styles.recommendedTitle}>Recommended</Text>
+                        <FlatList
+                            data={recommended}
+                            renderItem={({ item }) =>
+                                <RecommendItem item={item} size={130} onPress={() => navigation.navigate('Detail',
+                                    { name: item.name, image: item.image, info: item.info, description: item.description, actors: item.actors })} />}
+                            showsHorizontalScrollIndicator={false}
+                            keyExtractor={({ id }) => id}
+                            horizontal={true}
+                        />
+                    </View>
+                </View>}
         </SafeAreaView>
     );
 }
